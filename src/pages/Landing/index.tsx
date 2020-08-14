df4082ce4b0640d747a93e776504d56f4f5a426e
Lucas Thomaz <lucasthomazr@hotmail.com>
2020-08-14 09:18:29 -0300

    Base app done

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 import LandingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -12,6 +13,16 @@ import styles from './styles';
 
 const Landing: React.FC = () => {
   const { navigate } = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
   
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
@@ -49,7 +60,7 @@ const Landing: React.FC = () => {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 345 conexões já realixadas {' '}
+        Total de {totalConnections} conexões já realixadas {' '}
         <Image source={hearIcon} />
       </Text>
     </View>
